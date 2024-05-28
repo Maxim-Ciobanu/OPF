d4f34661bcf86759e410524552dfdef0a89c031a
davidjpeters <davidpeters019@gmail.com>
2024-05-28 12:00:27 -0600

    add include functions again cause i forgot one lol

--- MPOPF/Problem-1_Case-5.jl
+++ MPOPF/Problem-1_Case-5.jl
@@ -3,67 +3,11 @@ using Ipopt
 using JuMP
 const PM = PowerModels
 
+include("functions.jl")
+
 file_path = "./Cases/case5.m"
 
 #a function to deal with two different time variables (done by gpt using Sajad's code)
-function run_optimization(data)
-    ref = PowerModels.build_ref(data)[:it][:pm][:nw][0]
-
-    model = JuMP.Model(Ipopt.Optimizer)
-
-    @variable(model, va[i in keys(ref[:bus])])
-
-    @variable(model, ref[:gen][i]["pmin"] <= pg[i in keys(ref[:gen])] <= ref[:gen][i]["pmax"])
-
-    @variable(model, -ref[:branch][l]["rate_a"] <= p[(l,i,j) in ref[:arcs_from]] <= ref[:branch][l]["rate_a"])
-
-    p_expr = Dict([((l,i,j), 1.0*p[(l,i,j)]) for (l,i,j) in ref[:arcs_from]])
-    p_expr = merge(p_expr, Dict([((l,j,i), -1.0*p[(l,i,j)]) for (l,i,j) in ref[:arcs_from]]))
-
-    @objective(model, Min, sum(gen["cost"][1]*pg[i]^2 + gen["cost"][2]*pg[i] + gen["cost"][3] for (i,gen) in ref[:gen]))
-
-    for (i,bus) in ref[:ref_buses]
-        @constraint(model, va[i] == 0)
-    end
-
-    for (i,bus) in ref[:bus]
-        # Build a list of the loads and shunt elements connected to the bus i
-        bus_loads = [ref[:load][l] for l in ref[:bus_loads][i]]
-        bus_shunts = [ref[:shunt][s] for s in ref[:bus_shunts][i]]
-
-        # Active power balance at node i
-        @constraint(model,
-            sum(p_expr[a] for a in ref[:bus_arcs][i]) ==    
-            sum(pg[g] for g in ref[:bus_gens][i]) -                 # sum of active power generation at bus i -
-            sum(load["pd"] for load in bus_loads) -                 # sum of active load consumption at bus i -
-            sum(shunt["gs"] for shunt in bus_shunts)*1.0^2          # sum of active shunt element injections at bus i
-        )
-    end
-
-    # Branch power flow physics and limit constraints
-    for (i,branch) in ref[:branch]
-        # Build the from variable id of the i-th branch, which is a tuple given by (branch id, from bus, to bus)
-        f_idx = (i, branch["f_bus"], branch["t_bus"])
-
-        p_fr = p[f_idx]                     # p_fr is a reference to the optimization variable p[f_idx]
-
-        va_fr = va[branch["f_bus"]]         # va_fr is a reference to the optimization variable va on the from side of the branch
-        va_to = va[branch["t_bus"]]         # va_fr is a reference to the optimization variable va on the to side of the branch
-
-        # Compute the branch parameters and transformer ratios from the data
-        g, b = PowerModels.calc_branch_y(branch)
-
-        # DC Power Flow Constraint
-        @constraint(model, p_fr == -b*(va_fr - va_to))
-
-        # Voltage angle difference limit
-        @constraint(model, va_fr - va_to <= branch["angmax"])
-        @constraint(model, va_fr - va_to >= branch["angmin"])
-    end
-
-    optimize!(model)
-    return JuMP.value.(pg), objective_value(model)
-end
 
 # Time 1 optimization
 data_time1 = PowerModels.parse_file(file_path)
