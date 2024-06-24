4d5ffc3edbb7ea2a8e9be3f833a1082675fef0d5
Maxim Ciobanu <maxim.ciobanu.seller@gmail.com>
2024-06-24 10:13:12 -0600

    Outputed model variables in the main file and added new miscellaneous file for other functions.

--- OOP-Refactoring/src/main.jl
+++ OOP-Refactoring/src/main.jl
@@ -1,8 +1,9 @@
 using PowerModels, JuMP, Ipopt, Gurobi
 include("classes.jl")
+include("misc.jl")
 using .MPOPF
 
-file_path = "./Cases/case5.m"
+file_path = "./Cases/case14.m"
 
 
 # Example for AC
@@ -23,11 +24,7 @@ optimize_model(My_DC_model)
 
 # Example for AC with UncertaintyFactory
 # --------------------------------------------------------------------------
-load_scenarios_factors = Dict(
-    1 => Dict(1 => 1, 2 => 1, 3 => 1, 4 => 1, 5 => 1),
-    2 => Dict(1 => 1.001, 2 => 1.001, 3 => 1.003, 4 => 1.002, 5 => 1.001),
-    3 => Dict(1 => 0.98, 2 => 0.99, 3 => 0.997, 4 => 0.998, 5 => 0.99)
-)
+load_scenarios_factors = load_scenarios_factors = generate_load_scenarios(3, 14)
 # Using AC Factory from previous example
 My_AC_model_Uncertainty = create_model(ac_factory, load_scenarios_factors)
 optimize_model(My_AC_model_Uncertainty)
@@ -36,14 +33,15 @@ optimize_model(My_AC_model_Uncertainty)
 
 # Example for DC with UncertaintyFactory
 # --------------------------------------------------------------------------
-load_scenarios_factors2 = Dict(
-    1 => Dict(1 => 1, 2 => 1, 3 => 1, 4 => 1, 5 => 1),
-    2 => Dict(1 => 1.03, 2 => 1.03, 3 => 1.03, 4 => 1.03, 5 => 1.03),
-    3 => Dict(1 => 0.95, 2 => 0.95, 3 => 0.95, 4 => 0.95, 5 => 0.95)
-)
+load_scenarios_factors2 = load_scenarios_factors = generate_load_scenarios(3, 14)
 # Using DC Factory but with Gurobi
 dc_factory_Gurobi = DCPowerFlowModelFactory(file_path, Gurobi.Optimizer)
 My_DC_model_Uncertainty = create_model(dc_factory_Gurobi, load_scenarios_factors2)
 optimize_model(My_DC_model_Uncertainty)
 # --------------------------------------------------------------------------
-display(JuMP.value.(mu_minus))
+
+
+modelToAnalyse = My_DC_model_Uncertainty
+display(JuMP.value.(modelToAnalyse.model[:pg]))
+display(JuMP.value.(modelToAnalyse.model[:mu_plus]))
+display(JuMP.value.(modelToAnalyse.model[:mu_minus]))
