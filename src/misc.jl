57ccb32b7c265844d19e4d029662ca68b1b5acf1
inspizzz <wiktor.wiejak@gmail.com>
2024-08-02 11:08:54 -0600

    Created a universal struct class for graphing

--- src/misc.jl
+++ src/misc.jl
@@ -82,28 +82,110 @@ function output_to_file(output::String, file_name::String="")
 	end
 end
 
-# Function for plotting the objective cost against solver iterations
+
+# struct for storing data about the graph
+#
+# plot: Any - the plotly plot
+# traces: Vector{GenericTrace} - array of traces to be plotted
+# location: String - save location
+# ------------------------------------------------------------
+mutable struct Graph
+	plot::Any
+	traces::Vector{GenericTrace} # array of traces to be plotted
+	location::String # save location
+
+	# constructor for the Graph struct
+	function Graph(location::String)
+		print("creating function with variables")
+		new(nothing, Vector{GenericTrace}(), location)
+	end
+end
+
+
+# a function for adding a scatter plot to the graph
+# 
+# graph: Graph - the graph to add the scatter plot to
 # x: Array{Int} - x-axis values
 # y: Array{Int} - y-axis values
-# x_label: String - x-axis label
-# y_label: String - y-axis label
-# -------------------------------------------------------------
-function save_graph(x::Array, y::Array, x_label::String, y_label::String, location::String)
-	trace = scatter(x=x, y=y,
+# name: String - name of the trace ( optional )
+# color: String - color of the trace ( optional )
+# ------------------------------------------------------------
+function add_scatter(graph::Graph, x::Array, y::Array, name::String="default name", color::String="blue")
+	trace = PlotlyJS.scatter(x=x, y=y,
 		mode="lines+markers",
-		name="Objective Cost",
-		marker_color="blue",
+		name=name,
+		marker_color=color,
 		hoverinfo="x+y", # Ensure hover displays both x and y values
 		hovertemplate="%{x}, %{y:.2f}<extra></extra>") # Custom hover text format
 
-	layout = Layout(
-		title="Plotting Objective Cost agaist Solver Iterations",
-		xaxis=attr(title=x_label, tickangle=-45, tickmode="linear", tick0=0, dtick=1),
-		yaxis=attr(title=y_label, hoverformat=".2f"),
-		showlegend=true)
-
-	My_plot = plot([trace], layout)
-	PlotlyJS.savefig(My_plot, location)
+	push!(graph.traces, trace)
 end
 
-save_graph([1, 2, 3, 4, 5], [1, 2, 3, 4, 5], "X label", "Y label", "output/graphs/graph.png")
+
+# a function for creating the plot with 
+#
+# graph: Graph - the graph to be created from the traces
+# title: String - title of the plot
+# x_label: String - x-axis label
+# y_label: String - y-axis label
+# ------------------------------------------------------------
+function create_plot(graph::Graph, title::String, x_label::String, y_label::String)
+
+	# conditions
+	if (length(graph.traces) < 1) throw("cannot create plot, no traces to plot") end
+
+	# create the layout
+	layout = Layout(
+		title=title,
+		xaxis=attr(title=x_label, tickangle=-45, tickmode="linear", tick0=0, dtick=1),
+		yaxis=attr(title=y_label, hoverformat=".2f"),
+		showlegend=true
+	)
+
+	# create the plot from the traces and the layout
+	print(graph.traces)
+	graph.plot = plot(graph.traces, layout)
+end
+
+
+# a function for saving a plot that is currently being create_model
+#
+# graph: Graph - the graph to be saved
+# ------------------------------------------------------------
+function save_graph(graph::Graph)
+	
+	print(graph.location)
+	# minimum conditions to be met before saving
+	if (graph.location == "") 
+		throw("cannot save, location is not set") 
+	end
+	if (isnothing(graph.plot)) 
+		throw("cannot save, plot is not set")
+	end
+	if (length(graph.traces) < 1)
+		throw("cannot save, no traces to plot")
+	end
+
+	# save the plot in the location
+	PlotlyJS.savefig(graph.plot, graph.location)
+end
+
+
+# a function for displaying the graph
+#
+# graph: Graph - the graph to be displayed
+# ------------------------------------------------------------
+function display_graph(graph::Graph)
+
+	# check if the plot is set
+	if (!graph.plot) throw("cannot display, plot is not set") end
+
+	display(graph.plot)
+end
+
+
+graph = Graph("output/plot.html")
+print(graph)
+add_scatter(graph, [1, 2, 3, 4, 5], [1, 2, 3, 4, 5], "trace 1", "blue")
+create_plot(graph, "my plot", "x-axis", "y-axis")
+save_graph(graph)
