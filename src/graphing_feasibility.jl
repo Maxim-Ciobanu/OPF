0b9265d2d630931341eb7ad4f06b48d01a3f7847
Maxim Ciobanu <maxim.ciobanu.seller@gmail.com>
2024-08-26 11:49:52 -0600

    Created another generalised function that allows the user to specify which models to compare when checking feasibility. Authored by @inspizzz.

--- src/graphing_feasibility.jl
+++ src/graphing_feasibility.jl
@@ -1,6 +1,9 @@
-using JuMP, Ipopt
-using MPOPF
-
+using PowerModels, JuMP, Ipopt, Plots#, Gurobi
+include("MPOPF.jl")
+include("misc.jl")
+include("search_functions.jl")
+include("graphing_class.jl")
+using .MPOPF
 
 # create enum for linear models
 @enum MODEL_TYPE begin
@@ -10,14 +13,6 @@ using MPOPF
 	Lin3=3
 end
 
-# extract the raw file names from cases folder, then add the path to file_paths array
-file_strings = [string(i) for i in sort([parse(Int, join(filter(isdigit, collect(s)))) for s in readdir("./Cases")])]
-file_paths = map((x) -> join(["./Cases/case", x, ".m"]), file_strings)
-
-# create the graph object
-feasability_graph = Graph("output/graphs/feasibility.html")
-v_error_graph = Graph("output/graphs/v_error.html")
-o_error_graph = Graph("output/graphs/o_error.html")
 
 # create a function for retreiveing the data, if it exists
 # 
@@ -70,7 +65,6 @@ function retreive_data(factory::Union{ACMPOPFModelFactory, DCMPOPFModelFactory, 
 end
 
 
-
 # a general function for executing a specific model and checking its feasability
 # dc models have no vm values, so default to 1 and qg values are 0
 # 
@@ -91,6 +85,7 @@ function generalised(factory::Union{ACMPOPFModelFactory, DCMPOPFModelFactory, Li
 		v_error[path] = new_v_error
 		o_error[path] = new_o_error
 		
+		# return the data before any calculations are made
 		return costs, v_error, o_error
 	end
 
@@ -99,8 +94,8 @@ function generalised(factory::Union{ACMPOPFModelFactory, DCMPOPFModelFactory, Li
 	optimize_model(model)
 
 	# get the length of the buses
-	test_ref = get_ref(model.data)
-	bus_len = length(test_ref[:bus])
+	ref = get_ref(model.data)
+	bus_len = length(ref[:bus])
 
 	# extract the pg and qg values
 	pg = value.(model.model[:pg])
@@ -135,96 +130,157 @@ function generalised(factory::Union{ACMPOPFModelFactory, DCMPOPFModelFactory, Li
 	v_error[path] = new_v_error
 	o_error[path] = new_o_error
 
+	# output the v values from the optimized model
+	vm = ac_model.model[:vm]
+	for (i, branch) in ref[:branch]
+		vm_fr = vm[1,branch["f_bus"]]
+		vm_to = vm[1,branch["t_bus"]]
+
+		for (vi, vj) in zip(values.(vm_fr), values.(vm_to))
+			println(value(vi), " -> ", value(vj))
+			output_to_file("$(path) -> $(log(value(vi)) - log(value(vj)))"; file_name="v_values/vm_diff.txt")
+		end
+	end
+
 	return costs, v_error, o_error
 end
 
+"""
+	perform_feasibility(models::Array, finish_save::Bool=false)
 
-# initiate the different models arrays
-ac_models = [[], [], []]
-dc_models = [[], [], []]
-lin1_models = [[], [], []]
-lin2_models = [[], [], []]
-lin3_models = [[], [], []]
+# Fields
+- `models::Array` : an array of 5, where each element is a model to be performed
+	- 1 = AC, 2 = DC, 3 = Lin1, 4 = Lin2, 5 = Lin3
+	- toggle 1 = on, 0 = off
+- `finish_save::Bool=false` : a boolean to determine if the graphs should be saved
 
+# Description
+- This function allows the user to execute feasibility tests on the different models provided
+	- AC
+	- DC
+	- Lin1
+	- Lin2
+	- Lin3
+- for each case in the cases folder the function will loop over them and perform the feasibility tests
+for each model that has been toggled to be on
+"""
+function perform_feasibility(models::Array, finish_save::Bool=false)
 
-# loop through all the cases
-for path in file_paths
-	println("Case: ", path)
+	# extract the raw file names from cases folder, then add the path to file_paths array
+	file_strings = [string(i) for i in sort([parse(Int, join(filter(isdigit, collect(s)))) for s in readdir("./Cases")])]
+	file_paths = map((x) -> join(["./Cases/case", x, ".m"]), file_strings)
+
+	# create the graph object
+	feasability_graph = Graph("output/graphs/feasibility.html")
+	v_error_graph = Graph("output/graphs/v_error.html")
+	o_error_graph = Graph("output/graphs/o_error.html")
 	
-	# create the model factories
-	ac_factory = ACMPOPFModelFactory(path, Ipopt.Optimizer)
-	dc_factory = DCMPOPFModelFactory(path, Ipopt.Optimizer)
-	lin1_factory = LinMPOPFModelFactory(path, Ipopt.Optimizer)
-	lin2_factory = LinMPOPFModelFactory(path, Ipopt.Optimizer)
-	lin3_factory = LinMPOPFModelFactory(path, Ipopt.Optimizer)
+	# initiate the different models arrays
+	ac_models = [[], [], []]
+	dc_models = [[], [], []]
+	lin1_models = [[], [], []]
+	lin2_models = [[], [], []]
+	lin3_models = [[], [], []]
 
-	# get the cost of each model
-	cost_ac, v_error_ac, o_error_ac = generalised(ac_factory, path)
-	cost_dc, v_error_dc, o_error_dc = generalised(dc_factory, path)
-	cost_lin_regular, v_error_lin_regular, o_error_lin_regular = generalised(lin1_factory, path, Lin1)
-	cost_lin_quadratic, v_error_lin_quadratic, o_error_lin_quadratic = generalised(lin2_factory, path, Lin2)
-	cost_lin_log, v_error_lin_log, o_error_lin_log = generalised(lin3_factory, path, Lin3)
+	# loop through all the cases
+	for path in file_paths
+		println("Case: ", path)
 
-	# add the nodes to the graph
-	push!(ac_models[1], cost_ac); push!(ac_models[2], v_error_ac); push!(ac_models[3], o_error_ac)
-	push!(dc_models[1], cost_dc); push!(dc_models[2], v_error_dc); push!(dc_models[3], o_error_dc)
-	push!(lin1_models[1], cost_lin_regular); push!(lin1_models[2], v_error_lin_regular); push!(lin1_models[3], o_error_lin_regular)
-	push!(lin2_models[1], cost_lin_quadratic); push!(lin2_models[2], v_error_lin_quadratic); push!(lin2_models[3], o_error_lin_quadratic)
-	push!(lin3_models[1], cost_lin_log); push!(lin3_models[2], v_error_lin_log); push!(lin3_models[3], o_error_lin_log)
+		if (models[1] == 1)
+			# perform the AC model
+			ac_factory = ACMPOPFModelFactory(path, Ipopt.Optimizer)
+			cost_ac, v_error_ac, o_error_ac = generalised(ac_factory, path)
+			push!(ac_models[1], cost_ac); push!(ac_models[2], v_error_ac); push!(ac_models[3], o_error_ac)
+		end
+		
+		if (models[2] == 1)
+			# perform the DC model
+			dc_factory = DCMPOPFModelFactory(path, Ipopt.Optimizer)
+			cost_dc, v_error_dc, o_error_dc = generalised(dc_factory, path)
+			push!(dc_models[1], cost_dc); push!(dc_models[2], v_error_dc); push!(dc_models[3], o_error_dc)
+		end
+
+		if (models[3] == 1)
+			# perform the linearized models
+			lin1_factory = LinMPOPFModelFactory(path, Ipopt.Optimizer)
+			cost_lin_regular, v_error_lin_regular, o_error_lin_regular = generalised(lin1_factory, path, Lin1)
+			push!(lin1_models[1], cost_lin_regular); push!(lin1_models[2], v_error_lin_regular); push!(lin1_models[3], o_error_lin_regular)
+		end
+
+		if (models[4] == 1)
+			# perform the linearized models
+			lin2_factory = LinMPOPFModelFactory(path, Ipopt.Optimizer)
+			cost_lin_quadratic, v_error_lin_quadratic, o_error_lin_quadratic = generalised(lin2_factory, path, Lin2)
+			push!(lin2_models[1], cost_lin_quadratic); push!(lin2_models[2], v_error_lin_quadratic); push!(lin2_models[3], o_error_lin_quadratic)
+		end
+
+		if (models[5] == 1)
+			# perform the linearized models
+			lin3_factory = LinMPOPFModelFactory(path, Ipopt.Optimizer)
+			cost_lin_log, v_error_lin_log, o_error_lin_log = generalised(lin3_factory, path, Lin3)
+			push!(lin3_models[1], cost_lin_log); push!(lin3_models[2], v_error_lin_log); push!(lin3_models[3], o_error_lin_log)
+		end
+	end
+
+	# for each model
+	# save the model data to file
+	# add the feaibility, v_error and o_error to the graph
+	if (models[1] == 1)
+		save("output/feasability_saved/ac/feasability", merge(ac_models[1]...)); save("output/feasability_saved/ac/v_error", merge(ac_models[2]...)); save("output/feasability_saved/ac/o_error", merge(ac_models[3]...))
+		add_scatter(feasability_graph, file_strings, [collect(values(i))[1] for i in ac_models[1]], "AC", "blue")
+		add_scatter(v_error_graph, file_strings, [collect(values(i))[1] for i in ac_models[2]], "AC", "blue")
+		add_scatter(o_error_graph, file_strings, [collect(values(i))[1] for i in ac_models[3]], "AC", "blue")
+	end
+	
+	if (models[2] == 1)
+		save("output/feasability_saved/dc/feasability", merge(dc_models[1]...)); save("output/feasability_saved/dc/v_error", merge(dc_models[2]...)); save("output/feasability_saved/dc/o_error", merge(dc_models[3]...))
+		add_scatter(feasability_graph, file_strings, [collect(values(i))[1] for i in dc_models[1]], "DC", "red")
+		add_scatter(v_error_graph, file_strings, [collect(values(i))[1] for i in dc_models[2]], "DC", "red")
+		add_scatter(o_error_graph, file_strings, [collect(values(i))[1] for i in dc_models[3]], "DC", "red")
+	end
+
+	if (models[3] == 1)
+		save("output/feasability_saved/lin1/feasability", merge(lin1_models[1]...)); save("output/feasability_saved/lin1/v_error", merge(lin1_models[2]...)); save("output/feasability_saved/lin1/o_error", merge(lin1_models[3]...))
+		add_scatter(feasability_graph, file_strings, [collect(values(i))[1] for i in lin1_models[1]], "Lin1", "green")
+		add_scatter(v_error_graph, file_strings, [collect(values(i))[1] for i in lin1_models[2]], "Lin1", "green")
+		add_scatter(o_error_graph, file_strings, [collect(values(i))[1] for i in lin1_models[3]], "Lin1", "green")
+	end
+
+	if (models[4] == 1)
+		save("output/feasability_saved/lin2/feasability", merge(lin2_models[1]...)); save("output/feasability_saved/lin2/v_error", merge(lin2_models[2]...)); save("output/feasability_saved/lin2/o_error", merge(lin2_models[3]...))
+		add_scatter(feasability_graph, file_strings, [collect(values(i))[1] for i in lin2_models[1]], "Lin2", "yellow")
+		add_scatter(v_error_graph, file_strings, [collect(values(i))[1] for i in lin2_models[2]], "Lin2", "yellow")
+		add_scatter(o_error_graph, file_strings, [collect(values(i))[1] for i in lin2_models[3]], "Lin2", "yellow")
+	end
+
+	if (models[5] == 1)
+		save("output/feasability_saved/lin3/feasability", merge(lin3_models[1]...)); save("output/feasability_saved/lin3/v_error", merge(lin3_models[2]...)); save("output/feasability_saved/lin3/o_error", merge(lin3_models[3]...))
+		add_scatter(feasability_graph, file_strings, [collect(values(i))[1] for i in lin3_models[1]], "Lin3", "purple")
+		add_scatter(v_error_graph, file_strings, [collect(values(i))[1] for i in lin3_models[2]], "Lin3", "purple")
+		add_scatter(o_error_graph, file_strings, [collect(values(i))[1] for i in lin3_models[3]], "Lin3", "purple")
+	end
+
+	# create a function for scanning the data
+	# this will for each case, evaluate all previous cases from that point 
+	# to see which model performs best for all previous cases
+
+	# create layouts
+	create_plot(feasability_graph, "Feasibility of Various Linearized Models", "Cases", "Costs")
+	create_plot(v_error_graph, "Voltage Magnitude ( Vm ) Error of Various Models", "Cases", "Magnitude Error")
+	create_plot(o_error_graph, "Voltage Angle ( Va ) Error of Various Models", "Cases", "Angle Error")
+
+	# save the graphs
+	if finish_save
+		save_graph(feasability_graph)
+		save_graph(v_error_graph)
+		save_graph(o_error_graph)
+	end
+
+	# return the graph
+	return feasability_graph, v_error_graph, o_error_graph
 end
 
-# save the data to the output folder
-save("output/feasability_saved/ac/feasability", merge(ac_models[1]...)); save("output/feasability_saved/ac/v_error", merge(ac_models[2]...)); save("output/feasability_saved/ac/o_error", merge(ac_models[3]...))
-save("output/feasability_saved/dc/feasability", merge(dc_models[1]...)); save("output/feasability_saved/dc/v_error", merge(dc_models[2]...)); save("output/feasability_saved/dc/o_error", merge(dc_models[3]...))
-save("output/feasability_saved/lin1/feasability", merge(lin1_models[1]...)); save("output/feasability_saved/lin1/v_error", merge(lin1_models[2]...)); save("output/feasability_saved/lin1/o_error", merge(lin1_models[3]...))
-save("output/feasability_saved/lin2/feasability", merge(lin2_models[1]...)); save("output/feasability_saved/lin2/v_error", merge(lin2_models[2]...)); save("output/feasability_saved/lin2/o_error", merge(lin2_models[3]...))
-save("output/feasability_saved/lin3/feasability", merge(lin3_models[1]...)); save("output/feasability_saved/lin3/v_error", merge(lin3_models[2]...)); save("output/feasability_saved/lin3/o_error", merge(lin3_models[3]...))
-
-# create a function for scanning the data
-# this will for each case, evaluate all previous cases from that point 
-# to see which model performs best for all previous cases
-
-# add the traces to the feasability graph
-add_scatter(feasability_graph, file_strings, [collect(values(i))[1] for i in ac_models[1]], "AC", "blue")
-add_scatter(feasability_graph, file_strings, [collect(values(i))[1] for i in dc_models[1]], "DC", "red")
-add_scatter(feasability_graph, file_strings, [collect(values(i))[1] for i in lin1_models[1]], "Lin1", "green")
-add_scatter(feasability_graph, file_strings, [collect(values(i))[1] for i in lin2_models[1]], "Lin2", "yellow")
-add_scatter(feasability_graph, file_strings, [collect(values(i))[1] for i in lin3_models[1]], "Lin3", "purple")
-
-# add the traces to the v_error graph
-add_scatter(v_error_graph, file_strings, [collect(values(i))[1] for i in ac_models[2]], "AC", "blue")
-# add_scatter(v_error_graph, file_strings, [collect(values(i))[1] for i in dc_models[2]], "DC", "red")
-add_scatter(v_error_graph, file_strings, [collect(values(i))[1] for i in lin1_models[2]], "Lin1", "green")
-add_scatter(v_error_graph, file_strings, [collect(values(i))[1] for i in lin2_models[2]], "Lin2", "yellow")
-add_scatter(v_error_graph, file_strings, [collect(values(i))[1] for i in lin3_models[2]], "Lin3", "purple")
-
-# add the traces to the o_error graph
-add_scatter(o_error_graph, file_strings, [collect(values(i))[1] for i in ac_models[3]], "AC", "blue")
-add_scatter(o_error_graph, file_strings, [collect(values(i))[1] for i in dc_models[3]], "DC", "red")
-add_scatter(o_error_graph, file_strings, [collect(values(i))[1] for i in lin1_models[3]], "Lin1", "green")
-add_scatter(o_error_graph, file_strings, [collect(values(i))[1] for i in lin2_models[3]], "Lin2", "yellow")
-add_scatter(o_error_graph, file_strings, [collect(values(i))[1] for i in lin3_models[3]], "Lin3", "purple")
-
-# create layouts
-create_plot(feasability_graph, "Feasibility of Various Linearized Models", "Cases", "Costs")
-create_plot(v_error_graph, "Voltage Magnitude ( Vm ) Error of Various Models", "Cases", "Magnitude Error")
-create_plot(o_error_graph, "Voltage Angle ( Va ) Error of Various Models", "Cases", "Angle Error")
-
-# save the graphs
-save_graph(feasability_graph)
-save_graph(v_error_graph)
-save_graph(o_error_graph)
-
-display(feasability_graph.plot)
-
-#=
-println(file_paths[1])
-ac_factory = ACMPOPFModelFactory(file_paths[1], Ipopt.Optimizer)
-if retreive_data(ac_factory, file_paths[1]) != false
-	feasability, v_error, o_error = retreive_data(ac_factory, file_paths[1])
-	println(feasability)
-	println(v_error)
-	println(o_error)
-	println("found")
-end
-=#
+# takes an array of 5
+# 1 = AC, 2 = DC, 3 = Lin1, 4 = Lin2, 5 = Lin3
+# toggle 1 = on, 0 = off
+graph1, gaph2, graph3 = perform_feasibility([0, 0, 0, 0, 1])
