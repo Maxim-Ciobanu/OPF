cf9b5a6e6d9a3bc6ddfdb01fe48ce069a43ae2a0
inspizzz <wiktor.wiejak@gmail.com>
2024-09-29 16:59:31 +0100

    Fixed error when violations or feasibility constraints is empty in the main file.

--- src/main.jl
+++ src/main.jl
@@ -26,18 +26,21 @@ for case in keys(models)
 
 		average_violation = total_violation / number_violations
 
-		for (con, val) in feasibility
-			println("")
-			println("Infeasible constraint: ", con)
-			println("Current value: ", val)
-			println("")
+		# look into the feasbility violations
+		if length(feasibility) > 0
+			for (con, val) in feasibility
+				println("")
+				println("Infeasible constraint: ", con)
+				println("Current value: ", val)
+				println("")
+			end
 		end
 
-		println("Max violation: ", maximum(getindex.(values(violations), 4)))
-		println("Average violation: ", average_violation)
-		# catch
-		# 	println("No infeasible constraints found")
-		# end
+		# look into the bound violations below
+		if length(violations) > 0
+			println("Max violation: ", maximum(getindex.(values(violations), 4)))
+			println("Average violation: ", average_violation)
+		end
 	end
 end
 
